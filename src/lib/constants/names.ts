9a736800f44e382071f55e3acd14c76cc22f269c
Tymek <vonautymek@gmail.com>
2025-05-07 16:26:27 +0200

    added welcome screen

---
+++ src/lib/constants/names.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = 'Agin Chat';
+export const APP_TAGLINE =
+    "Agin Chat is a simple, focused Matrix client that gets out of your way. It's built for fast, reliable messaging across all your rooms and spaces — no clutter, no distractions.";
--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,5 +1,8 @@
 import { SplashSection } from '@/components/SplashScreen';
+import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
+import { IconArrowRight } from '@tabler/icons-react';
+import { APP_NAME, APP_TAGLINE } from '@/lib/constants/names';
 
 export const Route = createFileRoute('/welcome')({
     component: RouteComponent,
@@ -8,8 +11,13 @@ export const Route = createFileRoute('/welcome')({
 function RouteComponent() {
     return (
         <SplashSection>
-            <div className="font-semibold text-2xl mb-2">Agin Chat</div>
-            <div className="text-sm text-muted-foreground">Connecting to matrix.org</div>
+            <div className="font-semibold text-2xl mb-2">{APP_NAME}</div>
+            <div className="text-sm text-muted-foreground w-2xl text-center">{APP_TAGLINE}</div>
+
+            <Button className="mt-4">
+                Get Started
+                <IconArrowRight />
+            </Button>
         </SplashSection>
     );
 }
