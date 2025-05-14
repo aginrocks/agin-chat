32284186cc12e473cf60841d92c07f116e59cfdf
Tymek <vonautymek@gmail.com>
2025-05-14 19:34:39 +0200

    added input auto focus

--- src/components/chat/message-input/index.tsx
+++ src/components/chat/message-input/index.tsx
@@ -3,8 +3,15 @@ import { IconMoodSmile, IconGif, IconSend2, IconSticker } from '@tabler/icons-re
 import TextareaAutosize from 'react-textarea-autosize';
 import { AttachButton } from './attach-button';
 import { getHotkeyHandler } from '@mantine/hooks';
+import { useEffect, useRef } from 'react';
+import { useStartTyping } from 'react-use';
 
 export function MessageInput() {
+    const ref = useRef<HTMLTextAreaElement>(null);
+
+    useStartTyping(() => ref.current?.focus());
+    useEffect(() => ref.current?.focus(), []);
+
     return (
         <div className="border border-white/5 rounded-lg flex bg-secondary-hover items-start">
             <div className="p-1 pr-0">
@@ -15,6 +22,7 @@ export function MessageInput() {
                 placeholder="Message Room Name"
                 onKeyDown={getHotkeyHandler([['Enter', (e) => e.preventDefault()]])}
                 maxRows={10}
+                ref={ref}
             />
             <div className="flex p-1 pl-0">
                 <Button variant="ghost" size="icon" className="dark:hover:bg-secondary-hover-2">
