72cc6ba925ecf83e23252d73e6846465fa1ea70d
Tymek <vonautymek@gmail.com>
2025-05-20 09:30:24 +0200

    fixed scrollbars

--- src/components/chat/room-timeline.tsx
+++ src/components/chat/room-timeline.tsx
@@ -13,7 +13,11 @@ export function RoomTimeline({ roomId }: RoomTimelineProps) {
     const groupedMessages = useGroupEvents(timeline.events);
 
     return (
-        <ScrollArea className="flex-1 h-1">
+        <ScrollArea
+            className="flex-1 h-1"
+            type="always"
+            scrollbarProps={{ className: 'mx-2 py-2' }}
+        >
             <div className="flex flex-col gap-2 pb-3 pt-6">
                 {groupedMessages.map((group, index) => (
                     <MessageGroup key={`group-${index}-${group[0]?.data.getId()}`} data={group} />
