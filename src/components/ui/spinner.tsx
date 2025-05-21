9d549d5758182d899b5ce657efe5aab98f6abfdb
Tymek <vonautymek@gmail.com>
2025-05-21 10:57:29 +0200

    changed spinner

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,31 +1,46 @@
-import { cn } from '@lib/utils';
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
 
-// TODO: Make a better spinner
-export type SpinnerProps = {
+import { cn } from '@/lib/utils';
+
+const spinnerVariants = cva('flex-col items-center justify-center', {
+    variants: {
+        show: {
+            true: 'flex',
+            false: 'hidden',
+        },
+    },
+    defaultVariants: {
+        show: true,
+    },
+});
+
+const loaderVariants = cva('animate-spin text-primary', {
+    variants: {
+        size: {
+            small: 'size-6',
+            medium: 'size-8',
+            large: 'size-12',
+        },
+    },
+    defaultVariants: {
+        size: 'medium',
+    },
+});
+
+interface SpinnerContentProps
+    extends VariantProps<typeof spinnerVariants>,
+        VariantProps<typeof loaderVariants> {
     className?: string;
-};
+    children?: React.ReactNode;
+}
 
-export function Spinner({ className }: SpinnerProps) {
+export function Spinner({ size, show, children, className }: SpinnerContentProps) {
     return (
-        <svg
-            className={cn('animate-spin h-5 w-5 text-muted-foreground', className)}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-        >
-            <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-            ></circle>
-            <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-        </svg>
+        <span className={spinnerVariants({ show })}>
+            <Loader2 className={cn(loaderVariants({ size }), className)} />
+            {children}
+        </span>
     );
 }
